dcf4dedfa1285826494dbd8618270c24d0fe79e2
DANIS <danis.preldzic@sqa-consulting.com>
2024-05-24 09:20:41 +0200

    initial commit

---
+++ TodoListSwiftUI/TodoListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListSwiftUIApp.swift
+//  TodoListSwiftUI
+//
+//  Created by Danis Preldzic on 24. 5. 2024..
+//
+
+import SwiftUI
+
+@main
+struct TodoListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
