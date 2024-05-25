9f15445ba9e008a7fa839c451b7d51ca03244a06
DANIS <danis.preldzic@sqa-consulting.com>
2024-05-26 00:17:04 +0200

    item model converted to codable added user defaults for items

--- TodoListSwiftUI/ViewModels/ListViewModel.swift
+++ TodoListSwiftUI/ViewModels/ListViewModel.swift
@@ -10,31 +10,41 @@ import Foundation
 
 class ListViewModel: ObservableObject {
     
-    @Published var items: [ItemModel] = []
+    @Published var items: [ItemModel] = [] {
+        // on every change of this array of objects, this will trigger
+        didSet {
+            saveItemToDefaults()
+        }
+    }
+    
+    let items_key = "ITEMS_KEY"
     
     
     init() {
-       getInitialData()
+        getInitialData()
     }
     
     func getInitialData() {
-        let newItems: [ItemModel] = [
-            ItemModel(title: "This is first item!", isChecked: false),
-            ItemModel(title: "This is second!", isChecked: true),
-            ItemModel(title: "Third", isChecked: false)
-        ]
-        items.append(contentsOf: newItems)
+        // check user defaults and decode the data if there is any
+        guard
+            let data = UserDefaults.standard.data(forKey: items_key),
+            let savedItems = try? JSONDecoder().decode([ItemModel].self, from: data)
+        else {
+            return
+        }
+        
+        items.append(contentsOf: savedItems)
     }
     
     
     func delete(indexSet: IndexSet) {
         items.remove(atOffsets: indexSet)
     }
-
+    
     func move(indexSet: IndexSet, newOffset: Int) {
         items.move(fromOffsets: indexSet, toOffset: newOffset)
     }
-
+    
     func addItem(title: String) {
         let newItem = ItemModel(title: title, isChecked: false)
         items.append(newItem)
@@ -55,6 +65,12 @@ class ListViewModel: ObservableObject {
             items[index] = item.updateCompletion()
         }
     }
-   
+    
+    func saveItemToDefaults() {
+        if let encodedData = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.set(encodedData, forKey: items_key)
+        }
+    }
+    
     
 }
